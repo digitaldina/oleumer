659635c5fba07cd328b88b36056c55f420483e33
digitaldina <dinaelhanan@gmail.com>
2022-01-16 08:43:13 -0500

    add code for site along with comments

--- oleumerSite.py
+++ oleumerSite.py
@@ -1,24 +1,20 @@
 
-from flask import Flask, render_template                 #import
-def home():                                              #function call
-
-    return render_template('home.html')                  #return and calling HTML page (designed template)
-
+from flask import Flask, render_template                 #imports              
  
 #functions for calling, processing the model and prediction function
 def load_image(img_path):
 
     img = image.load_img(img_path, target_size=(224, 224))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+    img_tensor = image.img_to_array(img)                    
+    img_tensor = np.expand_dims(img_tensor, axis=0)         
+    img_tensor /= 255.                                      
 
     return img_tensor
 
 def get_model():
     global model
     model = load_model('model.h5')
-    print("Model loaded!")
+    print("loaded!")
 
 def prediction(img_path):
     new_image = load_image(img_path)
@@ -40,15 +36,16 @@ def prediction(img_path):
         return "Good"
 
 
-app = Flask(__name__)                                    #calling
+app = Flask(__name__)                                    
 get_model()
 
+#returning and calling web page
+@app.route("/", methods=['GET', 'POST'])                 
+def home():                                              
 
-@app.route("/", methods=['GET', 'POST'])                 #initialising
-def home():                                              #function call
+    return render_template('home.html')                  
 
-    return render_template('home.html')                  #return and calling HTML page (designed template)
-    
+#calling predications and print result
 @app.route("/predict", methods = ['GET','POST'])
 def predict():
     
@@ -56,13 +53,14 @@ def predict():
         
         file = request.files['file']
         filename = file.filename
-        file_path = os.path.join(r'C:/Users/nEW u/Flask/static/', filename)                       #slashes should be handeled properly
+        file_path = os.path.join(r'C:/Users/nEW u/Flask/static/', filename)                       
         file.save(file_path)
         print(filename)
         product = prediction(file_path)
         print(product)
-        
-    return render_template('predict.html', product = product, user_image = file_path)            #file_path can or may used at the place of filename
+
+# you can use file_path instead of filename
+    return render_template('predict.html', product = product, user_image = file_path)           
 
 if __name__ == "__main__":
     app.run() 
